afeb9168eb338d095852ceb16adf9578895fc9f7
shubhamsingh4 <87186754+shubhamsingh4@users.noreply.github.com>
2021-07-20 16:42:13 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,40 +2,48 @@ var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
 
-var mario, mario_running, mario_collided;
+var trex, trex_running, trex_collided;
 var ground, invisibleGround, groundImage;
 
-var coinGroup, coinImage;
-var obstaclesGroup, obstacle2, obstacle1,obstacle3;
-var score=0;
-var life=3;
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
- 
+var score=0;
 
 var gameOver, restart;
 
-localStorage["HighestScore"] = 0;
+
 
 function preload(){
-  mario_running = loadAnimation("Capture1.png","Capture3.png","Capture4.png");
-  mario_collided = loadAnimation("mariodead.png");
-  groundImage = loadImage("backg.jpg");
-  coinSound = loadSound("coin.wav")
-  coinImage = loadImage("coin.png");
-  obstacle2 = loadImage("obstacle2.png");
+  trex_running =   loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
   obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
   obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
   gameOverImg = loadImage("gameOver.png");
   restartImg = loadImage("restart.png");
 }
 
 function setup() {
   createCanvas(600, 200);
-  mario = createSprite(50,180,20,50);
-  mario.addAnimation("running", mario_running);
-  mario.scale = 0.5;
   
-  ground = createSprite(0,190,1200,10);
+  trex = createSprite(50,180,20,50);
+  
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided", trex_collided);
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
   
@@ -51,120 +59,116 @@ function setup() {
   gameOver.visible = false;
   restart.visible = false;
   
-  coinGroup = new Group();
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  cloudsGroup = new Group();
   obstaclesGroup = new Group();
   
   score = 0;
-  life = 3;
 }
 
 function draw() {
-  background("blue");
-  textSize(20);
-  fill(255);
-  text("Score: "+ score, 500,40);
-  text("life: "+ life , 500,60);
-  drawSprites();
+  //trex.debug = true;
+  background(255);
+  text("Score: "+ score, 500,50);
+  
   if (gameState===PLAY){
-   
-    if(score >= 0){
-      ground.velocityX = -6;
-    }else{
-      ground.velocityX = -(6 + 3*score/100);
+    score = score + Math.round(getFrameRate()/60);
+    ground.velocityX = -(6 + 3*score/100);
+  
+    if(keyDown("space") && trex.y >= 159) {
+      trex.velocityY = -12;
     }
   
-    if(keyDown("space") && mario.y >= 139) {
-      mario.velocityY = -15;
-    }
-  
-    mario.velocityY = mario.velocityY + 0.8
+    trex.velocityY = trex.velocityY + 0.8
   
     if (ground.x < 0){
       ground.x = ground.width/2;
     }
   
-    mario.collide(ground);
-    
-    spawnCoin();
+    trex.collide(invisibleGround);
+    spawnClouds();
     spawnObstacles();
   
-   if(obstaclesGroup.isTouching(mario)){
-        life = life-1;
+    if(obstaclesGroup.isTouching(trex)){
         gameState = END;
-    } 
-    if (mario.isTouching( coinGroup)){
-        score = score+1; 
-        coinGroup[0].destroy();
-        coinSound.play();
     }
-    
   }
-  
-  else if (gameState === END ) {
+  else if (gameState === END) {
     gameOver.visible = true;
     restart.visible = true;
-    mario.addAnimation("collided", mario_collided);
     
     //set velcity of each game object to 0
     ground.velocityX = 0;
-    mario.velocityY = 0;
+    trex.velocityY = 0;
     obstaclesGroup.setVelocityXEach(0);
-    coinGroup.setVelocityXEach(0);
+    cloudsGroup.setVelocityXEach(0);
     
     //change the trex animation
-    mario.changeAnimation("collided",mario_collided);
-    mario.scale =0.35;
+    trex.changeAnimation("collided",trex_collided);
     
     //set lifetime of the game objects so that they are never destroyed
     obstaclesGroup.setLifetimeEach(-1);
-    coinGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
     
-    if(mousePressedOver(restart) && life>0 ) {
+    if(mousePressedOver(restart)) {
       reset();
     }
   }
+  
+  
+  drawSprites();
 }
 
-function spawnCoin() {
+function spawnClouds() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var coin = createSprite(600,120,40,10);
-    coin.y = Math.round(random(80,120));
-    coin.addImage(coinImage);
-    coin.scale = 0.1;
-    coin.velocityX = -3;
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
     
      //assign lifetime to the variable
-    coin.lifetime = 200;
+    cloud.lifetime = 200;
     
     //adjust the depth
-    coin.depth = mario.depth;
-    mario.depth = mario.depth + 1;
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
     
     //add each cloud to the group
-    coinGroup.add(coin);
+    cloudsGroup.add(cloud);
   }
   
 }
 
 function spawnObstacles() {
   if(frameCount % 60 === 0) {
-    var obstacle = createSprite(600,165,10,40);    
+    var obstacle = createSprite(600,165,10,40);
+    //obstacle.debug = true;
+    obstacle.velocityX = -(6 + 3*score/100);
+    
     //generate random obstacles
-    var rand = Math.round(random(1,3));
+    var rand = Math.round(random(1,6));
     switch(rand) {
-      case 1: obstacle.addImage(obstacle2);
+      case 1: obstacle.addImage(obstacle1);
               break;
-      case 2: obstacle.addImage(obstacle1);
+      case 2: obstacle.addImage(obstacle2);
               break;
       case 3: obstacle.addImage(obstacle3);
               break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
     }
-        
-    obstacle.velocityX = -(6 + 3*score/100);
     
     //assign scale and lifetime to the obstacle           
-    obstacle.scale = 0.2;
+    obstacle.scale = 0.5;
     obstacle.lifetime = 300;
     //add each obstacle to the group
     obstaclesGroup.add(obstacle);
@@ -177,14 +181,11 @@ function reset(){
   restart.visible = false;
   
   obstaclesGroup.destroyEach();
-  coinGroup.destroyEach();
+  cloudsGroup.destroyEach();
   
-  mario.changeAnimation("running",mario_running);
-  mario.scale =0.5;
+  trex.changeAnimation("running",trex_running);
   
-  if(localStorage["HighestScore"]<score){
-    localStorage["HighestScore"] = score;
-  }
+ 
   
   score = 0;
   
